f5dd9f64b070abc072fd6e4de86f0f74c08198f2
Satyaban Mahakud <satyaban.mahakud@rediffmail.com>
2018-10-06 03:51:49 +0530

    Added dockerfiles and Nginx project information

--- check_super_user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $UID -ne 0 ]; then
- echo Not root user. Please run as root
-else
- echo Root user
-fi
--- printf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# filename : printf.sh
-
-
-printf "%-5s %-10s %-4s\n" No Name Mark
-printf "%-5s %-10s %-4.2f\n" 1 Sarath 0.3456
-printf "%-5s %-10s %-4.2f\n" 2 James 90.9989
-printf "%-5s %-10s %-4.2f\n" 3 Jeff 77.765
