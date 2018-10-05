373cb90964fd8f7cc093ea523760b5448aca02d3
Satyaban Mahakud <satyaban.mahakud@rediffmail.com>
2018-10-05 13:12:44 +0000

    added new files

---
+++ add.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+no1=4;
+no2=5;
+
+let result=no1+no2
+echo result
---
+++ printf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# filename : printf.sh
+
+
+printf "%-5s %-10s %-4s\n" No Name Mark
+printf "%-5s %-10s %-4.2f\n" 1 Sarath 0.3456
+printf "%-5s %-10s %-4.2f\n" 2 James 90.9989
+printf "%-5s %-10s %-4.2f\n" 3 Jeff 77.765
